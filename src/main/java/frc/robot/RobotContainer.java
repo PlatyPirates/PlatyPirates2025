8e7c0b532c6ac7d42405a0d0de31c38a73868b73
FRC-PlatyPirates-9181 <team@platypirates.org>
2025-01-27 19:56:18 -0600

    Changed speeds and added buttons for elementary STEMfest

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -63,8 +63,8 @@ public class RobotContainer {
         // Turning is controlled by the X axis of the right stick.
         new RunCommand(
             () -> m_robotDrive.drive(
-                -MathUtil.applyDeadband(m_driverController.getLeftY()*0.3, OIConstants.kDriveDeadband),
-                MathUtil.applyDeadband(m_driverController.getLeftX()*0.3, OIConstants.kDriveDeadband),
+                -MathUtil.applyDeadband(m_driverController.getLeftY()*0.2, OIConstants.kDriveDeadband),
+                -MathUtil.applyDeadband(m_driverController.getLeftX()*0.2, OIConstants.kDriveDeadband),
                 -MathUtil.applyDeadband(m_driverController.getRightX()*0.5, OIConstants.kDriveDeadband),
                 true, true),
             m_robotDrive));
@@ -80,10 +80,15 @@ public class RobotContainer {
    * {@link JoystickButton}.
    */
   private void configureButtonBindings() {
-    new JoystickButton(m_operatorController, Button.kCross.value)
+    new JoystickButton(m_operatorController, Button.kR1.value)
         .whileTrue(new RunCommand(
             () -> m_robotDrive.setX(),
             m_robotDrive));
+
+    new JoystickButton(m_operatorController, Button.kL1.value)
+        .whileTrue(new RunCommand(
+          () -> m_robotDrive.zeroHeading(),
+          m_robotDrive));
   }
 
   /**
